f81444686e966d8a4fff36fe473f041125215186
kanghory <132531297+kanghory@users.noreply.github.com>
2023-06-12 21:09:53 +0700

    Update and rename senmenu.sh to senxray.sh

---
+++ senxray.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# // config Data
+echo -e "${GREEN}Config Data${NC}"
+
+
+wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/kanghory/VPN/main/add-ws.sh"
+wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/kanghory/VPN/main/add-ssws.sh"
+#wget -q -O /usr/bin/add-socks "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/add-socks.sh"
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/kanghory/VPN/main/add-vless.sh"
+wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/kanghory/VPN/main/add-tr.sh"
+wget -q -O /usr/bin/add-trgo "https://raw.githubusercontent.com/kanghory/VPN/main/add-trgo.sh"
+
+
+
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/kanghory/VPN/main/menu-vless.sh"
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/kanghory/VPN/main/menu-vmess.sh"
+
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/kanghory/VPN/main/menu-ss.sh"
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/kanghory/VPN/main/menu-trojan.sh"
+
+
+wget -q -O /usr/bin/updatsc "https://raw.githubusercontent.com/kanghory/VPN/main/update.sh"
+wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/kanghory/VPN/main/backup.sh"
+wget -q -O /usr/bin/renel "https://raw.githubusercontent.com/kanghory/VPN/main/renel.sh"
+wget -q -O /usr/bin/cekl "https://raw.githubusercontent.com/kanghory/VPN/main/cekl.sh"
+wget -q -O /usr/bin/dell "https://raw.githubusercontent.com/kanghory/VPN/main/dell.sh"
+
+chmod +x /usr/bin/menu-backup
+#chmod +x /usr/bin/menu
+chmod +x /usr/bin/add-ws
+chmod +x /usr/bin/add-ssws
+#chmod +x /usr/bin/add-socks
+chmod +x /usr/bin/add-vless
+chmod +x /usr/bin/add-tr
+chmod +x /usr/bin/addl
+chmod +x /usr/bin/cekl
+chmod +x /usr/bin/dell
+chmod +x /usr/bin/renel
+chmod +x /usr/bin/usernew
+chmod +x /usr/bin/autoreboot
+chmod +x /usr/bin/addhost
+chmod +x /usr/bin/genssl
+chmod +x /usr/bin/restarts
+chmod +x /usr/bin/tendang
+chmod +x /usr/bin/clearlog
+chmod +x /usr/bin/running
+chmod +x /usr/bin/cek-trafik
+chmod +x /usr/bin/cek-speed
+chmod +x /usr/bin/cek-bandwidth
+chmod +x /usr/bin/cek-ram
+chmod +x /usr/bin/limit-speed
+chmod +x /usr/bin/menu-vless
+chmod +x /usr/bin/menu-vmess
+chmod +x /usr/bin/menu-ss
+chmod +x /usr/bin/updatsc
+chmod +x /usr/bin/slowdns
+chmod +x /usr/bin/menu-trojan
+chmod +x /usr/bin/trialssh
+chmod +x /usr/bin/menun-ssh
+chmod +x /usr/bin/menu-backup
+chmod +x /usr/bin/menu
+chmod +x /usr/bin/backup
+chmod +x /usr/bin/wbm
+chmod +x /usr/bin/xp
+chmod +x /usr/bin/changer
+chmod +x /usr/bin/fix
+#chmod +x /usr/bin/update
+
+
+
+
